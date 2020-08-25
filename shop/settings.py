5f86274674e5e5fd4042db588c52b904942acdc7
hany <you@example.com>
2020-08-25 21:23:45 +0200

    third commit

--- shop/settings.py
+++ shop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_URL='/static/'
